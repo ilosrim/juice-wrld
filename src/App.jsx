95e0c162f3a15c5f8df0b9be41ada214a8799d5b
ilosrim <ilosrim@yahoo.com>
2021-12-03 19:25:28 +0500

    End project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe
+        title="Juice WRLD Music Player"
+        src="https://open.spotify.com/embed/artist/4MCBfE4596Uoi2O4DtmEMz"
+        width="350" height="450px"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media">
+      </iframe>
+    </div>
+  );
+}
+
+export default App;
